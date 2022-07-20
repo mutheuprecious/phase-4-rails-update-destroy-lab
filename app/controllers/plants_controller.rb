8935688f4c75bfb8891c8051a6c9f0cac11eae5a
Precious <preciousmaregwa1997@gmail.com>
2022-07-20 23:59:16 +0300

    commit tree a30070a2cc19138513b790d2ab04fad3a91e6bcc

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,47 @@
 class PlantsController < ApplicationController
 
-  # GET /plants
-  def index
-    plants = Plant.all
-    render json: plants
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    # GET /plants
+    def index
+      plants = Plant.all
+      render json: plants
+    end
+  
+    # GET /plants/:id
+    def show
+      plant = render_plant
+      render json: plant
+    end
+  
+    # POST /plants
+    def create
+      plant = Plant.create(plant_params)
+      render json: plant, status: :created
+    end
+  
+    def update
+      plant = render_plant
+      plant.update(plant_params)
+      render json: plant
+    end
+  
+    def destroy
+      plant = render_plant
+      plant.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def plant_params
+      params.permit(:name, :image, :price, :is_in_stock)
+    end
+  
+    def render_plant
+      Plant.find(params[:id])
+    end
+  
+    def render_not_found
+      render json: { error: "Plant not found" }, status: :not_found
+    end
   end
-
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
-
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
-  end
-
-  private
-
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
-end
